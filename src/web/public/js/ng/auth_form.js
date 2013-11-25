f2a7a0128b9cdd37c475a1fb3734af81b3389a0a
Rozhnov Alexandr <nox73@ya.ru>
2013-11-25 22:43:58 +0400

    Improved auth form

--- src/web/public/js/ng/auth_form.js
+++ src/web/public/js/ng/auth_form.js
@@ -3,8 +3,16 @@ function AuthForm($scope) {
   $scope.login = ''
   $scope.password = ''
 
+  if(localStorage.login) {$scope.login = localStorage.login}
+  if(localStorage.password) {$scope.password = localStorage.password}
+
   $scope.sendAuth = function () {
+    localStorage.login = $scope.login
+    localStorage.password = $scope.password
+
     $scope.sendMessage( JSON.stringify({ Type:"Auth", Login: $scope.login, Password: $scope.password }) )
   }
+
+  if($scope.login != '' && $scope.password != ''){ setTimeout( $scope.sendAuth, 1000 ) }
 
 }
